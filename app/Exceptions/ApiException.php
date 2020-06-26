6b6c0e9738be1bca687702cef1e46d1d0505f423
wanglei <317448423@qq.com>
2020-06-26 18:39:46 +0800

    更新错误提示

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -7,45 +7,20 @@ use App\Helpers\ApiResponse;
 
 class ApiException extends Exception
 {
-    use ApiResponse;
-    /**
-     * 将异常渲染至 HTTP 响应值中。
-     *
-     * @param  \Illuminate\Http\Request
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request)
-    {
-        $error = static::setError();
-        if(isset($error[$this->getMessage()])){
-            return $this->failed($this->getMessage());
-        }else{
-            return $this->failed($this->getMessage(),59999);
-        }
-    }
-
-    protected static function setError(){
-        return [
-            21000 => trans('auth.failed'), //账号密码错误
-            21001 => trans('auth.emptyPermissions'), //控权限报错
-
-            21002 => trans('form.addError'), // 添加错误
-            21003 => trans('form.editError'), // 编辑错误
-
-            21004 => trans('auth.authError'), //权限错误
-            21005 => trans('auth.statusError'), //用户被禁用
-
-            21006 => trans('auth.exceptionLogin'), //异常登录
-            21007 => trans('auth.usernameUnique'), //用户名重复
-
-            21008 => trans('auth.menuUnique'), //名称重复,
-            21009 => trans('auth.oldPasswordError'),//原始密码不正确
-            21010 => trans('auth.configUnique'), //配置名称重复
-            21011 => trans('auth.dictionaryUnique'), //字典名称重复
-            21022 => '分类别名重复',
-            21023 => '课程已经存在',
-            21024 => '线下课程已经存在',
-            21025 => 'VIP ID不正确'
-        ];
-    }
+	use ApiResponse;
+	/**
+	 * 将异常渲染至 HTTP 响应值中。
+	 *
+	 * @param  \Illuminate\Http\Request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request)
+	{
+		$error = static::setError();
+		if(isset($error[$this->getMessage()])){
+			return $this->failed($this->getMessage());
+		}else{
+			return $this->failed($this->getMessage(),59999);
+		}
+	}
 }
